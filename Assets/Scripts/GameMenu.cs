dd16c5b74bc3b1e85453615dad9f1a2a393b966e
jokubas <bombinbomber@gmail.com>
2025-12-05 02:28:11 +0000

    sound is set up

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -10,21 +10,35 @@ public class GameMenu : MonoBehaviour
     public GameObject ControlsScreen;
     public GameObject ExitLevelScreenButton;
 
+    public AudioSource source;
+    public AudioClip pop;
+
     private void Start()
     {
         LevelSelectScreen.SetActive(false);
         ControlsScreen.SetActive(false);
     }
 
+    public void ButtonPopSound()
+    {
+        if(source != null)
+        {
+            source.PlayOneShot(pop);
+        }
+        //source.PlayOneShot(pop);
+    }
+
 
     public void LevelSelect()
     {
+        source.PlayOneShot(pop);
         LevelSelectScreen.SetActive(true);
         MenuScreen.SetActive(false);
     }
 
     public void ExitLevelSelect()
     {
+        source.PlayOneShot(pop);
         LevelSelectScreen.SetActive(false);
         MenuScreen.SetActive(true);
         ControlsScreen.SetActive(false);
@@ -33,17 +47,21 @@ public class GameMenu : MonoBehaviour
 
     public void StartLevelOne()
     {
+        source.PlayOneShot(pop);
         SceneManager.LoadScene(0);
     }
 
     public void StartLevelTwo()
     {
+        source.PlayOneShot(pop);
         SceneManager.LoadScene(2);
     }
 
     public void Controls()
     {
+        source.PlayOneShot(pop);
         ControlsScreen.SetActive(true);
         MenuScreen.SetActive(false);
     }
+
 }
